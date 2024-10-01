d0c105ad9edf45b2e3550d7bcd38febf6ce5ba8b
Christen dijoux <christen.dijoux@gmail.com>
2024-10-01 14:37:12 -0400

    Initial commit with cleaned files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mon-portfolio/', // Utilisez le nom du dépôt ici, pour que GitHub Pages sache où chercher les ressources
+  build: {
+    outDir: 'dist', // Assurez-vous que le dossier de sortie est 'dist'
+  },
 })
